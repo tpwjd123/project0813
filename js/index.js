6e0ebdf2b271024751681f2c22822d497ed2a457
tpwjd123 <tpwjdskql@gmail.com>
2025-07-24 17:49:22 +0900

    검색창추가 및 스와이퍼 적용

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@ document.querySelector('button').addEventListener('click',(e)=>{
 e.preventDefault;
 document.querySelector('.popup').style.display='none';
 })
+
+  var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
