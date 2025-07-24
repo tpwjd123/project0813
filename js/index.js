00b7235d4ed1d6fdb885f1fffd2350259ea6c2a5
tpwjd123 <tpwjdskql@gmail.com>
2025-07-24 23:45:18 +0900

    스와이퍼 추가 및 검색창 추가

--- js/index.js
+++ js/index.js
@@ -1,21 +1,58 @@
-document.querySelector('button').addEventListener('click',(e)=>{
-e.preventDefault;
-document.querySelector('.popup').style.display='none';
+document.querySelector('button').addEventListener('click', (e) => {
+  e.preventDefault;
+  document.querySelector('.popup').style.display = 'none';
 })
 
-  var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 0,
-      centeredSlides: true,
-      autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+let swiper = new Swiper(".main_Swiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let swiperSection1 = new Swiper(".promotion .section1_sw", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  centeredSlides: true,
+  navigation: {
+    prevEl: ".swiper-prev",
+    nextEl: ".swiper-next",
+  },
+});
+
+let swiperSection3 = new Swiper(".promotion .section3_sw", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  centeredSlides: true,
+  navigation: {
+    prevEl: ".swiper-prev",
+    nextEl: ".swiper-next",
+  },
+});
+
+let swiperBanner = new Swiper(".banner .banner_swiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
